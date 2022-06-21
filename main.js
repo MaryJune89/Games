faa9a5f098306c04c20ede8ad3308261c5b45b0b
Mary June <98911428+MaryJune89@users.noreply.github.com>
2022-06-21 12:14:26 -0400

    Card game using a Card Deck API

--- main.js
+++ main.js
@@ -1,40 +1,61 @@
-// Player name
-let player1 = "Player 1";
-let player2 = "Player 2";
+//on page load fetch, gets deckID
+let deckId = ''
 
-// Function to change the player name
-function editNames() {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change player2 name");
+fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    deckId = data.deck_id
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  });
 
-    document.querySelector("p.Player1").innerHTML = player1;
-    document.querySelector("p.Player2").innerHTML = player2;
+document.querySelector('button').addEventListener('click', drawTwo) //clicks button, draws 2 cards
+
+function drawTwo() {
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2` //uses deckID to draw cards
+
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('#player1').src = data.cards[0].image //puts image of card into DOM
+      document.querySelector('#player2').src = data.cards[1].image
+
+      let player1Val = convertToNum(data.cards[0].value) //comparison below to see who won or declare war
+      let player2Val = convertToNum(data.cards[1].value)
+      if (player1Val > player2Val) {
+        document.querySelector('h3').innerText = ('Player 1 Wins!')
+      } else if (player1Val < player2Val) {
+        document.querySelector('h3').innerText = ('Player 2 Wins!')
+      } else {
+        document.querySelector('h3').innerText = ('I DECLARE WAR!!')
+      }
+
+
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+
+
+}
+function convertToNum(val) {     //helper function that converts any value into a number from string
+  if (val === 'ACE') {
+    return 14
+  } else if (val === 'KING') {
+    return 13
+  } else if (val === 'QUEEN') {
+    return 12
+  } else if (val === 'JACK') {
+    return 11
+  } else {
+    return Number(val) //converts strings to numbers
+  }
 }
 
-// Function to roll the dice
-function rollTheDice() {
-    setTimeout(function () {
-        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-        document.querySelector(".img1").setAttribute("src",
-            "dice" + randomNumber1 + ".png");
 
-        document.querySelector(".img2").setAttribute("src",
-            "dice" + randomNumber2 + ".png");
 
-        if (randomNumber1 === randomNumber2) {
-            document.querySelector("h1").innerHTML = "Draw!";
-        }
 
-        else if (randomNumber1 < randomNumber2) {
-            document.querySelector("h1").innerHTML
-                = (player2 + " WINS!");
-        }
-
-        else {
-            document.querySelector("h1").innerHTML
-                = (player1 + " WINS!");
-        }
-    }, 2500);
-
-}
